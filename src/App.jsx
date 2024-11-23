dda1132b68b8f4ce099d81a4cdd975eb7cc1c3f5
john.mukenge <john.mukenge@emeal.nttdata.com>
2024-11-23 12:23:04 +0100

    setup project - adding fonts, audio img and videos, tailwind config

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <main>
+            <h1 className="text-5xl text-violet-300 font-bold font-circular-web">Welcome to Awwwards</h1>
+        </main>
+    )
+}
+export default App;
